545cd3ebd7a1ec9f04f5159514f9b639df34cbcb
amagdykhalil <am2592379@gmail.com>
2026-08-02 20:56:43 +0300

    Update GTMPageView

--- src/components/atoms/GTMPageView.jsx
+++ src/components/atoms/GTMPageView.jsx
@@ -1,24 +1,21 @@
 'use client';
 
 import { useEffect } from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
+import { usePathname} from 'next/navigation';
 
 export default function GTMPageView() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
-
     window.dataLayer = window.dataLayer || [];
-      
+
     window.dataLayer.push({
       event: 'pageview',
       page_path: pathname,
       page_location: window.location.href,
       page_title: document.title,
-      page_query: searchParams.toString(),
     });
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   return null;
 }
